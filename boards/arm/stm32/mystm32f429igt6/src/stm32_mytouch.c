08f94c6bd8247d435f6ab680536fc6d2cba695ad
zeki_zhao <zeki_zhao@163.com>
2026-05-08 23:45:31 +0800

    更新mytouch，删除无用注释，优化初始化过程。

--- boards/arm/stm32/mystm32f429igt6/src/stm32_mytouch.c
+++ boards/arm/stm32/mystm32f429igt6/src/stm32_mytouch.c
@@ -3,24 +3,17 @@
 #include <nuttx/signal.h>
 #include <debug.h>
 #include <assert.h>
-#include <errno.h>
 #include "stm32.h"
 #include "mystm32f429igt6.h"
-// #include <nuttx/simulative_i2c/simulative_i2c.h>
 #include <arch/board/board.h>
 
-
 #include <nuttx/config.h>
-#include <stdint.h>
 #include <stdbool.h>
 #include <debug.h>
 
 #include <nuttx/arch.h>
 #include <nuttx/i2c/i2c_master.h>
 #include <nuttx/input/gt9xx.h>
-
-// #include "stm32_i2c.h"
-// #include "/home/zeki/workspace/NuttX/nuttxspace/nuttx/arch/arm/src/stm32/stm32_i2c_v2.c"
 
 #ifdef CONFIG_MY_TOUCH
 
@@ -65,29 +58,6 @@ static xcpt_t g_isr;
 static void *g_arg;
 static int my_gt9xx_irq_attach(const struct gt9xx_board_s *state,xcpt_t isr, void *arg)
 {
-  // int ret;
-
-  // iinfo("\n");
-  // DEBUGASSERT(state != NULL && isr != NULL && arg != NULL);
-
-  // /* Attach the Interrupt Handler to Port PH */
-
-  // ret = irq_attach(A64_IRQ_PH_EINT, isr, arg);
-  // if (ret < 0)
-  //   {
-  //     ierr("Attach Interrupt Handler failed: %d\n", ret);
-  //     return ret;
-  //   }
-
-  // /* Set Interrupt Priority in Generic Interrupt Controller v2 */
-
-  // arm64_gic_irq_set_priority(A64_IRQ_PH_EINT, 0, IRQ_TYPE_EDGE);
-
-  // /* Enable Interrupts for Port PH */
-
-  // up_enable_irq(A64_IRQ_PH_EINT);
-  // return OK;
-    
   wlinfo("Attach IRQ\n");
   g_isr = isr;
   g_arg = arg;
@@ -113,43 +83,6 @@ static int my_gt9xx_irq_attach(const struct gt9xx_board_s *state,xcpt_t isr, voi
 static void my_gt9xx_irq_enable(const struct gt9xx_board_s *state,
                                        bool enable)
 {
-  // int ret;
-
-  // iinfo("enable=%d\n", enable);
-  // DEBUGASSERT(state != NULL);
-
-  // if (enable)
-  //   {
-  //     /* Configure the Touch Panel Interrupt */
-
-  //     ret = a64_pio_config(CTP_INT);
-  //     if (ret < 0)
-  //       {
-  //         ierr("Configure Touch Panel Interrupt failed: %d\n", ret);
-  //         return;
-  //       }
-
-  //     /* Enable the Touch Panel Interrupt */
-
-  //     ret = a64_pio_irqenable(CTP_INT);
-  //     if (ret < 0)
-  //       {
-  //         ierr("Enable Touch Panel Interrupt failed: %d\n", ret);
-  //         return;
-  //       }
-  //   }
-  // else
-  //   {
-  //     /* Disable the Touch Panel Interrupt */
-
-  //     ret = a64_pio_irqdisable(CTP_INT);
-  //     if (ret < 0)
-  //       {
-  //         ierr("Disable Touch Panel Interrupt failed: %d\n", ret);
-  //         return;
-  //       }
-  //   }
-  // struct gt9xx_board_s *priv = (struct gt9xx_board_s *)state;
     irqstate_t flags;
 
     /* Attach and enable, or detach and disable.  Enabling and disabling GPIO
@@ -227,24 +160,18 @@ void board_touch_initialize(void)
             stm32_i2cbus_uninitialize(i2c);
         }
     }
-//     int ret;
-    syslog(LOG_DEBUG,"in %s:%d\n",__func__,__LINE__);
 
     stm32_configgpio(GPIO_TOUCH_RST);
-    stm32_configgpio(GPIO_TOUCH_INT1); //TODO:配置触控芯片没有生效？
     //DELAY
     sleep(1); //TODO:大于100us
     stm32_gpiowrite(GPIO_TOUCH_RST, 1);
     //DELAY
     sleep(1); //TODO:大于5ms
     stm32_configgpio(GPIO_TOUCH_INT2);
-    // syslog(LOG_DEBUG,"in %s:%d\n",__func__,__LINE__);
 
     DEBUGASSERT(devpath != NULL && i2c != NULL);
     gt9xx_register(devpath, i2c, TOUCH_ADDR, &my_gt9xx);
-    // register_driver("/dev/mytouch", &g_gt9xx_fileops, 0666, &test);
 
-    // syslog(LOG_DEBUG,"in %s:%d\n",__func__,__LINE__);
 }
 
 #endif
